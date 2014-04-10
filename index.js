85cde04fa05e4148791526f892c5ece35f353c02
Richard Walker <digitalsadhu@gmail.com>
2014-04-11 00:49:31 +0200

    Format file

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var writable    = require('stream').Writable
   , ws          = writable({ objectMode: true })
 
 ws._write = function (chunk, enc, next) {
-    process.env[chunk.key] = chunk.value
-    next()
+  process.env[chunk.key] = chunk.value
+  next()
 };
 
 module.exports = ws
