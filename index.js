b23d4fec86be4ea09858c065fc69778054def4d3
Richard Walker <digitalsadhu@gmail.com>
2014-04-12 19:24:45 +0200

    Move some functions back out of module exports

--- index.js
+++ index.js
@@ -3,52 +3,52 @@
 var through = require('through')
   , util    = require('util')
 
+var getFormat = function getFormat(data) {
+  if (util.isArray(data)) {
+    if (util.isArray(data[0]))
+      return 'multi-array'
+    else
+      return 'array'
+  }
+  if (typeof data === 'object') {
+    if (data.key && data.value)
+      return 'keyed-object';
+    else
+      return 'object'
+  }
+  return false
+}
+
+var setEnv = function setEnv(data, format) {
+
+  if (format === 'keyed-object') {
+    process.env[data.key] = data.value
+    return true
+  }
+
+  if (format === 'object') {
+    var keys = Object.keys(data)
+    keys.forEach(function (key) {
+      process.env[key] = data[key]
+    })
+  }
+
+  if (format === 'multi-array') {
+    data.forEach(function (envArr) {
+      process.env[envArr[0]] = envArr[1]
+    })
+    return true
+  }
+
+  if (format === 'array') {
+    process.env[data[0]] = data[1]
+    return true
+  }
+
+  return false
+}
+
 module.exports = function () {
-
-  var getFormat = function getFormat(data) {
-    if (util.isArray(data)) {
-      if (util.isArray(data[0]))
-        return 'multi-array'
-      else
-        return 'array'
-    }
-    if (typeof data === 'object') {
-      if (data.key && data.value)
-        return 'keyed-object';
-      else
-        return 'object'
-    }
-    return false
-  }
-
-  var setEnv = function setEnv(data, format) {
-
-    if (format === 'keyed-object') {
-      process.env[data.key] = data.value
-      return true
-    }
-
-    if (format === 'object') {
-      var keys = Object.keys(data)
-      keys.forEach(function (key) {
-        process.env[key] = data[key]
-      })
-    }
-
-    if (format === 'multi-array') {
-      data.forEach(function (envArr) {
-        process.env[envArr[0]] = envArr[1]
-      })
-      return true
-    }
-
-    if (format === 'array') {
-      process.env[data[0]] = data[1]
-      return true
-    }
-
-    return false
-  }
 
   var write = function write(data) {
 
